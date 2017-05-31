e684e0346b399a69cbebc6155d135badc74d1789
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2017-05-31 17:47:09 -0400

    updated testcases

--- AssortedAlogithms/src/testcases/AlgorithmTestCase.java
+++ AssortedAlogithms/src/testcases/AlgorithmTestCase.java
@@ -1,6 +1,7 @@
 package testcases;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import algorithms.InsertionSort;
 
@@ -10,7 +11,7 @@ public class AlgorithmTestCase {
 	public void insertionSortTest() {
 		int[] A = new int[]{5,3,1,7,20,45,16};
 		A = InsertionSort.sort(A);
-		//assertArrayEquals(new int[]{1,3,5,7,16,20,45},A);
+		assertArrayEquals(new int[]{1,3,5,7,16,20,45},A);
 	}
 
 }
