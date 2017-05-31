4b80fbeb1453db9e394f9e03cb879ae119d46a34
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2017-05-31 17:44:36 -0400

    updated insertion sort

--- AssortedAlogithms/src/algorithms/InsertionSort.java
+++ AssortedAlogithms/src/algorithms/InsertionSort.java
@@ -1,5 +1,18 @@
 package algorithms;
 
 public class InsertionSort {
+	
+	public static int[] sort(int[] A) {
+		for(int j = 1; j < A.length; j++) {
+			int key = A[j];
+			int i = j - 1;
+			while(i >= 0 && A[i] > key) {
+				A[i+1] = A[i];
+				i = i -1;
+			}
+			A[i+1] = key;
+		}
+		return A;
+	}
 
 }
