bc11c3538a2a89c71fef806d8329556a762aa3b2
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2017-06-01 19:19:08 -0400

    added square matrix multiply

--- AssortedAlogithms/src/testcases/AlgorithmTestCase.java
+++ AssortedAlogithms/src/testcases/AlgorithmTestCase.java
@@ -7,6 +7,7 @@ import algorithms.BubbleSort;
 import algorithms.InsertionSort;
 import algorithms.MaximumSubArray;
 import algorithms.MergeSort;
+import algorithms.SquareMatrix;
 
 public class AlgorithmTestCase {
 	
@@ -36,6 +37,20 @@ public class AlgorithmTestCase {
 		int[] A = new int[]{5,3,1,-9,20,45,16};
 		int[] maxSubArray = MaximumSubArray.find(A,0,A.length-1);
 		assertEquals(81,maxSubArray[2]);
+	}
+	
+	@Test
+	public void squareMatrixMultiplyTest() {
+		int[][] A = new int[][]{
+				{0,2},
+				{5,4}
+		};
+		int[][] B = new int[][]{
+				{1,9},
+				{7,6}
+		};
+		int[][] C = SquareMatrix.multiply(A,B);
+		assertArrayEquals(new int[][]{{14,12},{33,69}},C);
 	}
 
 }
