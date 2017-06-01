ebfe3c806ff7b356e2f1592fd4e0f4cd52d5e5ab
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2017-06-01 18:18:15 -0400

    added bubble sort

---
+++ AssortedAlogithms/src/algorithms/BubbleSort.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+public class BubbleSort {
+	
+	public static void sort(int[] A) {
+		for(int i = 0; i < A.length-1; i++) {
+			for(int j = A.length-1; j > i; j--) {
+				if(A[j] < A[j-1]) {
+					int temp = A[j];
+					A[j] = A[j-1];
+					A[j-1] = temp;
+				}
+			}
+		}
+	}
+	
+}
--- AssortedAlogithms/src/testcases/AlgorithmTestCase.java
+++ AssortedAlogithms/src/testcases/AlgorithmTestCase.java
@@ -3,6 +3,7 @@ package testcases;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import algorithms.BubbleSort;
 import algorithms.InsertionSort;
 import algorithms.MergeSort;
 
@@ -21,6 +22,13 @@ public class AlgorithmTestCase {
 		int p = 0;
 		int r = A.length-1;
 		MergeSort.sort(A,p,r);
+		assertArrayEquals(new int[]{1,3,5,7,16,20,45},A);
+	}
+	
+	@Test
+	public void bubbleSortTest() {
+		int[] A = new int[]{5,3,1,7,20,45,16};
+		BubbleSort.sort(A);
 		assertArrayEquals(new int[]{1,3,5,7,16,20,45},A);
 	}
 
