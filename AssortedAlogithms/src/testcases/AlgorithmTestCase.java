2501d2cad9d989024d876e6868a084790ed0eb2a
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2017-06-01 18:37:45 -0400

    updated mergeSortTestCase

--- AssortedAlogithms/src/testcases/AlgorithmTestCase.java
+++ AssortedAlogithms/src/testcases/AlgorithmTestCase.java
@@ -20,9 +20,7 @@ public class AlgorithmTestCase {
 	@Test
 	public void mergeSortTest() {
 		int[] A = new int[]{5,3,1,7,20,45,16};
-		int p = 0;
-		int r = A.length-1;
-		MergeSort.sort(A,p,r);
+		MergeSort.sort(A,0,A.length-1);
 		assertArrayEquals(new int[]{1,3,5,7,16,20,45},A);
 	}
 	
